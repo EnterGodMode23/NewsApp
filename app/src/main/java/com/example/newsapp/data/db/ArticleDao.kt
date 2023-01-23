961f4a7ccd60d53c9d96ac4c03d665172a38f2c7
AveMaria23 <demyn300@gmail.com>
2023-01-23 17:20:11 +0300

    Retrofit

---
+++ app/src/main/java/com/example/newsapp/data/db/ArticleDao.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.data.db
+
+import androidx.lifecycle.LiveData
+
+interface ArticleDao {
+    
+}
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "8fdb3fb378d64431b04b947be268d076"
+    }
+}
