45988f67377abc3bfec1ccd668d4a34c965197cc
AveMaria23 <demyn300@gmail.com>
2023-01-23 20:26:26 +0300

    Search view

--- app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
@@ -1,9 +1,15 @@
 package com.example.newsapp.data.api
 
+import androidx.room.Query
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(private val newsService: NewsService) {
 
     suspend fun getNews(countryCode: String, pageNumper: Int) =
         newsService.getHeadlines(countryCode = countryCode, page = pageNumper)
+
+
+    suspend fun getSearchNews(query: String, pageNumper: Int) =
+        newsService.getEverything(query = query, page = pageNumper)
+
 }
